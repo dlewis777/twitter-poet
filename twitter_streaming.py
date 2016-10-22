be62567b4e81b1002ccf796a74cae51eab648df3
David Li <zdavidli@users.noreply.github.com>
2016-10-22 01:50:17 -0400

    hillary and donald tweet data

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,18 +1,20 @@
 import tweepy
 import os
+import sys
 
 access_token = os.environ["twitter_akey"]
 access_secret = os.environ["twitter_asecret"]
 consumer_key = os.environ["twitter_ckey"]
 consumer_secret= os.environ["twitter_csecret"]
 #@realDonaldTrump id is 25073877
+#@HillaryClinton id is 1339835893
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-stuff = api.user_timeline(id = '25073877', count = 100, include_rts = False)
+stuff = api.user_timeline(id = sys.argv[1], count = 100, include_rts = False)
 
 for status in stuff:
     print status.text.encode('ascii', 'ignore')
