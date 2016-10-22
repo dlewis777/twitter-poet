81f7a30b6ae3f73130546a434f93ffe87e099857
dlewis777 <lefranchboi@gmail.com>
2016-10-22 14:26:06 -0400

    fixed push request

--- scripts/stringToNumber.py
+++ scripts/stringToNumber.py
@@ -1,11 +1,46 @@
 import string
 cmudict = {}
-
+tweetsent = []
 def todict(file):
 	f1 = open('cmudict.txt', 'r+w')
 	 
 	 for line in f1:
-	 	cmudict[line.split()[0]] = line.split()[1]
+	 	cmudict[line.split()[0]] = line.split()[1:]
+
+def tweet2chunk(tweet):
+
+	#strip links
+	tweet = tweet.split('https:')[0]
+	words = tweet.split()
+	periods = []
+	hashtag = []
+	for i in range(len(words)):
+
+		if '.' in words[i]:
+			periods.append(i)
+		if '#' in words[i]:
+			hashtag.append(i)	
+
+	if periods:
+		for i in periods:
+			words[i] = words[i].strip('.')
+			temp = words[0:i+1]
+			tweetsent.append(' '.join(temp))
+			for k in periods:
+				if k > i:
+					temp = words[i+1:k+1]
+					tweetsent.append(' '.join(temp))
+
+		for i in hashtag:
+			words[i] = words[i].strip('.')
+			temp = words[0:i+1]
+			tweetsent.append(' '.join(temp))
+			for k in periods:
+				if k < i:
+					temp = words[k+1: i+1]
+					tweetsent.append(' '.join(temp))
+
+	return tweetsent
 
 # Input a string that is equal to the text of a tweet.
 # Returns a string of numbers equal to the stresses of the letters
@@ -20,5 +55,5 @@ def l2n(chunk):
 			stress += phone
 
 	return stress
-	
+
 
